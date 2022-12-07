ee31eadcd4d8c580c0cfe56d56eaa5839231c4c4
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2022-12-07 09:13:25 +0200

    пробую 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(data) {
+  console.log(data);
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+player.setCurrentTime;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import { throttle } from 'lodash';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
 
+form.addEventListener('input', throttle(updateLocalStorage, 500));
+form.addEventListener('submit', handSubmit);
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+parseLocalStorage();
+function updateLocalStorage(e) {
+  const fiendName = e.target.name;
+  formData[fiendName] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function parseLocalStorage() {
+  const actualDate = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (actualDate) {
+    email.value = actualDate.email || '';
+    message.value = actualDate.message || '';
+  }
+}
+function handSubmit(e) {
+  e.preventDefaut();
+  const { email, message } = e.currentTarget.elemennts;
+  console.log({ email: email.value, message: message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
